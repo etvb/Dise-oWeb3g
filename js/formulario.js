a779089fa43d37014ae0edfac0370b603f1d4ed0
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2018-12-03 11:41:59 -0600

    creamos el clonado para crear los articulos

--- js/formulario.js
+++ js/formulario.js
@@ -12,9 +12,27 @@ function mostrarFormulario(){
 	return false;
 }
 
+function agregarPost(){
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		$clone = $post.clone();
+
+	$clone.find('.titulo_item a')
+		.text(titulo)
+		.attr('href',url);
+		$clone.hide();
+
+		$list.prepend($clone);
+
+		$clone.fadeIn();
+
+	return false;
+}
+
 
 
 
 
 	//eventos
-$button.click(mostrarFormulario);
+$button.on("click",mostrarFormulario);
+$form.on('submit', agregarPost);
